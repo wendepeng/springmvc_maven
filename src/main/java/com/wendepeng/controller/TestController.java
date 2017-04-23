bc520e5597dba15e22ab305483a29950f554c16a
闻德鹏 <wendepeng@renrenche.com>
2017-04-23 22:58:45 +0800

    揍你

--- src/main/java/com/wendepeng/controller/TestController.java
+++ src/main/java/com/wendepeng/controller/TestController.java
@@ -1,13 +1,23 @@
 package com.wendepeng.controller;
 
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TestController {
+public class TestController implements EnvironmentAware {
+
+    private Environment environment = null;
 
     @RequestMapping("/test")
     public String index(){
+        System.out.println(environment);
         return "demo";
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
